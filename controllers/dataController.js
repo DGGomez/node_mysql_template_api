10a3596e28a46cab8d9b87ce8de1db6137981a19
Daniel Gomez <dggomez21@gmail.com>
2019-03-03 07:28:30 -0500

    Update dataController.js

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -1,23 +1,43 @@
-
-
+var connectionInfo = require('../db/db.js');
+var mysql      = require('mysql');
 var connection = mysql.createConnection(connectionInfo);
 
 exports.list_all = function(req, res) {
-  
+  connection.query('SELECT * FROM events;', function(err,result){
+    if(err) throw err;
+    
+    return result;
+  });
 };
 
 exports.create = function(req, res) {
-  
+   connection.query(`INSERT INTO events (title, location, description) VALUES ($req.title, $req.location, $req.description);`, function(err,result){
+    if(err) throw err;
+    
+    return "Success";
+  });
 };
 
 exports.read = function(req, res) {
-   
+   connection.query(`Select * FROM events WHERE location=$req.location`, function(err,result){
+    if(err) throw err;
+    
+    return result;
+  });
 };
 
 exports.update = function(req, res) {
+   connection.query(`UPDATE events SET location=$req.location, description=$req.description WHERE title=$req.title;`, function(err,result){
+    if(err) throw err;
     
+    return "Success";
+  });
 };
 
 exports.delete = function(req, res) {
+    connection.query(`DELETE FROM events WHERE title=$req.title;`, function(err,result){
+    if(err) throw err;
     
+    return "Success";
+  });
 };
